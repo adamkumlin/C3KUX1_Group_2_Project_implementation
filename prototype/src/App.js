c3bbc07ecd00957e12a96cc64f3d9364fb9f9a53
adamkumlin <kumlinadam99@gmail.com>
2022-11-11 22:38:48 +0100

    add data

---
+++ prototype/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import data1 from './data1.json';
+import data2 from './data2.json';
+import data3 from './data3.json';
+import data4 from './data4.json';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
