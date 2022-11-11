01a56f0a4dc79f4e957b3cb5a7aa8ef5e6ccc379
adamkumlin <kumlinadam99@gmail.com>
2022-11-12 00:08:41 +0100

    add recharts and use on data

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,18 +1,28 @@
-import './App.css';
-import { Line } from 'react-chartjs-2';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
+
+const data1 = require('./data/data1.json');
+const data2 = require('./data/data2.json');
+const data3 = require('./data/data3.json');
+const data4 = require('./data/data4.json');
 
 function App() {
 
-  const data1 = require('./data/data1.json');
-  const data2 = require('./data/data2.json');
-  const data3 = require('./data/data3.json');
-  const data4 = require('./data/data4.json');
   return (
-    <div className="App">
-      <h1>Climate Change</h1>
-      
-      <Line data={data1} />
-    </div>
+    <LineChart width={700}height={700}data={data1}>
+
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="Year" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="Cement" stroke="green" activeDot={{ r: 8 }}/>
+      <Line type="monotone" dataKey="Gas Flaring" stroke="blue" activeDot={{ r: 8 }}/>
+      <Line type="monotone" dataKey="Gas Fuel" stroke="red" activeDot={{ r: 8 }} />
+      <Line type="monotone" dataKey="Liquid Fuel" stroke="gray" activeDot={{ r: 8 }}/>
+      <Line type="monotone" dataKey="Per Capita" stroke="purple" activeDot={{ r: 8 }}/>
+      <Line type="monotone" dataKey="Total" stroke="black" activeDot={{ r: 8 }}/>
+
+    </LineChart>
   );
 }
 
