489bd5dc9a917503191f8ad3345448b0d9f1984e
adamkumlin <kumlinadam99@gmail.com>
2022-12-13 13:06:38 +0100

    Update LoginForm.js

--- prototype/src/components/LoginForm.js
+++ prototype/src/components/LoginForm.js
@@ -1,19 +1,19 @@
 function LogInForm() {
 
-  return (
-    <div className="LogInForm">
-        <h2>Logga in</h2>
-        <div className="inputContainer">
-          <label>Användarnamn</label>
-          <input type="text" name="password"/>
-        </div>
-        <div className="inputContainer">
-          <label>Lösenord</label>
-          <input type="password" name="password"/>
-        </div>
-        <button>Logga in</button>
-    </div>
-  );
-}
-
-export default LogInForm;
+    return (
+      <div className="LogInForm">
+          <h2>Logga in</h2>
+          <div className="inputContainer">
+            <label>Användarnamn</label>
+            <input type="text" name="password"/>
+          </div>
+          <div className="inputContainer">
+            <label>Lösenord</label>
+            <input type="password" name="password"/>
+          </div>
+          <button>Logga in</button>
+      </div>
+    );
+  }
+  
+  export default LogInForm;
