ffd404d29ad537a30813a1441f0c2ada81da577b
adamkumlin <kumlinadam99@gmail.com>
2022-12-13 11:35:49 +0100

    align labels with inputs

--- prototype/src/components/LoginForm.js
+++ prototype/src/components/LoginForm.js
@@ -3,8 +3,14 @@ function LogInForm() {
   return (
     <div className="LogInForm">
         <h2>Logga in</h2>
-        <label>Användarnamn<input type="text" name="password"/></label> 
-        <label>Lösenord<input type="password" name="password"/></label>
+        <div className="inputContainer">
+          <label>Användarnamn</label>
+          <input type="text" name="password"/>
+        </div>
+        <div className="inputContainer">
+          <label>Lösenord</label>
+          <input type="password" name="password"/>
+        </div>
         <button>Logga in</button>
     </div>
   );
